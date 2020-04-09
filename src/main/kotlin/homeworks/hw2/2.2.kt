3f76368bc8a1bdafa07de7d5615e0d1575f44012
furetur <furetur@gmail.com>
2020-04-09 19:58:38 +0500

    2.2 fixed input bug

--- src/main/kotlin/homeworks/hw2/2.2.kt
+++ src/main/kotlin/homeworks/hw2/2.2.kt
@@ -8,7 +8,7 @@ fun <T> distinctRight(list: List<T>): List<T> {
 
 fun main() {
     println("Enter list and the program will remove all duplicate elements leaving only right occurrences")
-    val inputList = readLine()?.split(' ')?.map { it.toInt() } ?: throw IllegalArgumentException("Wrong input")
+    val inputList = readLine()?.split(' ') ?: throw IllegalArgumentException("Wrong input")
     val inputListDistinct = distinctRight(inputList)
 
     println("List without duplicate elements")
