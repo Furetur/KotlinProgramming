7b7cc4908887a265d06bc47c37ef1732fd4a2098
furetur <furetur@gmail.com>
2020-05-29 19:46:30 +0500

    nothing is done

--- src/main/kotlin/homeworks/tictactoe/GameClient.kt
+++ src/main/kotlin/homeworks/tictactoe/GameClient.kt
@@ -5,14 +5,16 @@ import io.ktor.client.features.websocket.WebSockets
 import io.ktor.client.features.websocket.ws
 import io.ktor.http.HttpMethod
 import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.WebSocketSession
+import io.ktor.http.cio.websocket.readText
 import io.ktor.util.KtorExperimentalAPI
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
-import tornadofx.launch
+import java.lang.IllegalArgumentException
 
 @KtorExperimentalAPI
 class GameClient {
     val client: HttpClient
+    var session: WebSocketSession? = null
+    var playerId: Int? = null
 
     companion object {
         const val URL = "ws://localhost:8080/"
@@ -24,18 +26,36 @@ class GameClient {
         }
     }
 
-    fun start() = coroutineScope {
+    suspend fun start() {
         client.ws(
             method = HttpMethod.Get,
             host = "127.0.0.1",
             port = 8080,
             path = "/"
         ) {
-            send(Frame.Text("dayum"))
+            session = this
             for (frame in incoming) {
-                print("Received: $frame")
+                if (frame is Frame.Text) {
+                    handleMessage(frame.readText())
+                }
             }
         }
+    }
+
+    fun handleMessage(message: String) {
+        when {
+            message.startsWith("gameStarted") ->
+        }
+    }
+
+    fun handleGameStart(message: String) {
+        val tokens = message.split(" ")
+        if (tokens.size < 2) {
+            throw IllegalArgumentException("Expected player id in gameStart message")
+        }
+        val receivedPlayerId = tokens[1].toIntOrNull()
+            ?: throw IllegalArgumentException("Expected player id of type Int in gameStart message")
+        playerId = receivedPlayerId
     }
 
     fun onTurn() {
