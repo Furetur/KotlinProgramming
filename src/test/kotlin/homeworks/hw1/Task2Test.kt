75a9c7ba58a95b4771e8ac715029ccea175b667d
furetur <furetur@gmail.com>
2020-06-18 23:33:21 +0500

    renamed test file

--- src/test/kotlin/homeworks/hw1/Task2Test.kt
+++ src/test/kotlin/homeworks/hw1/Task2Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import java.lang.IllegalArgumentException
 
-internal class TaskTest {
+internal class Task2Test {
 
     @Test
     fun factorialIterative_throwsExceptionOnNegativeNumbers() {
